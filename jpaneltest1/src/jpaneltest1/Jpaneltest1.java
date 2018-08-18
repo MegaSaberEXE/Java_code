9702e716e1e72e0eda2a0b23cc13d32f02fedf4c
MegaSaberEXE <chrishoangexe@gmail.com>
2018-08-18 16:04:39 -0500

    Add files via upload

---
+++ jpaneltest1/src/jpaneltest1/Jpaneltest1.java
@@ -0,0 +1,13 @@
+package jpaneltest1;
+
+
+public class Jpaneltest1 {
+
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null, "Hi");
+        System.exit(0);
+    }
+    
+}
+
+//Jpanel test successfully
